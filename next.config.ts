3a11b97e92f3e4d620188cf29e96592ac63152c7
Guillom <a7laszzzzzz@MacBook-Pro-de-Guillom.local>
2025-08-02 21:01:23 +0200

    fix: ajoute basePath pour GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/SwizzerProd' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/SwizzerProd' : '',
   images: {
     unoptimized: true,
   },
